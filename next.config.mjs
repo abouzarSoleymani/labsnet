cd9243e16c61946bbc3a0623218e3f228e3ac655
Abouzar Soleymani <iras.users@gmail.com>
2024-02-06 11:31:06 +0330

    enables static exports

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+};
 export default nextConfig;
